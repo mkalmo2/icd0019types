0d858025df38bbc0e37e7b3260a219f8522fde3f
Märt Kalmo <mart.kalmo@gmail.com>
2019-02-01 12:04:58 +0200

    added some classes

---
+++ src/types/Overload.java
@@ -0,0 +1,24 @@
+package types;
+
+public class Overload {
+
+    public static void main(String[] args) {
+        add(1L, 2L);
+    }
+
+    public static Long add(Long x, Long y) {
+        System.out.println("Adding longs");
+        return x + y;
+    }
+
+    public static Integer add(Integer x, Integer y) {
+        System.out.println("Adding integers");
+        return x + y;
+    }
+
+    public static Long add(String x, String y) {
+        System.out.println("Adding numbers from strings");
+        return Long.parseLong(x) + Long.parseLong(y);
+    }
+
+}

4e17fe7835f2bf99d02b2c173a605cceb0222fc6
Märt Kalmo <mart.kalmo@gmail.com>
2019-02-01 10:54:30 +0200

    1st

---
+++ src/types/Code.java
@@ -0,0 +1,33 @@
+package types;
+
+public class Code {
+
+    public static void main(String[] args) {
+
+        Integer[] numbers = new Integer[] {1, 3, -2, 9};
+
+        System.out.println(sum(numbers)); // 11
+    }
+
+    public static Integer sum(Integer[] numbers) {
+        return 0;
+    }
+
+    public static Double average(Integer[] numbers) {
+        return 0.0;
+    }
+
+    public static Integer minimumElement(Integer[] integers) {
+        return null;
+    }
+
+    public static String asString(Integer[] elements) {
+        return "";
+    }
+
+    public static String squareDigits(String s) {
+        return "";
+    }
+
+
+}

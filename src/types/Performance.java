be1a402a72e5714c354ccb5b029c3388abdec331
Märt Kalmo <mart.kalmo@gmail.com>
2024-02-07 11:18:23 +0200

    cleaned performance example

--- src/types/Performance.java
+++ src/types/Performance.java
@@ -6,12 +6,10 @@ public class Performance {
 
         double start = System.currentTimeMillis();
 
-        int x = 1;
-        int y = 2;
         double r = 0;
         for (int i = 0; i < 1e8; i++) {
-            r += (float) x / y;
-//            r += Float.valueOf(x) / y;
+            r += (float) i / 2;
+//            r += Float.valueOf(i) / 2;
         }
 
         System.out.println((System.currentTimeMillis() - start) / 1000);
